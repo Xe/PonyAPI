b8ded4cdcdf963bfd0b4c05700f6ba55b0f5b6c0
Christine Dodrill <xena@yolo-swag.com>
2015-08-12 18:16:33 -0700

    Add python tests

---
+++ client/python/test.py
@@ -0,0 +1,27 @@
+import ponyapi
+import unittest
+
+class TestPonyAPI(unittest.TestCase):
+    def test_newest(self):
+        ponyapi.newest()
+
+    def test_all_episodes(self):
+        assert len(ponyapi.all_episodes()) > 0
+
+    def test_last_aired(self):
+        ponyapi.last_aired()
+
+    def test_random(self):
+        ponyapi.random()
+
+    def test_get_season(self):
+        assert len(ponyapi.get_season(4)) > 0
+
+    def test_get_episode(self):
+        ponyapi.get_episode(2, 14)
+
+    def test_searcn(self):
+        assert len(ponyapi.search("pony")) > 0
+
+if __name__ == '__main__':
+    unittest.main()
