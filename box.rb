c2d040fad1e23ae03acb03d3e71fc8d30591c289
Christine Dodrill <me@christine.website>
2017-04-15 15:07:57 -0700

    reorganization

--- box.rb
+++ box.rb
@@ -3,29 +3,28 @@
 #
 # This is a manifest designed to be used with box[1]. Build it using:
 #
-#    $ box Boxfile
+#    $ box box.rb
 #
 # [1]: https://github.com/erikh/box
 
-from "xena/nim:0.15.2"
+from "xena/nim:0.16.0"
 
 env "BACKPLANE_PROXY_URL" => "http://127.0.0.1:5000"
 
 copy "ponyapi.nimble", "/app/ponyapi.nimble"
-copy "src",            "/app/"
-copy "public",         "/app/"
+copy "src",            "/app/src/"
+copy "public",         "/app/public/"
 copy "fim.list",       "/app/fim.list"
 
-workdir "/app"
-run "nimble update && yes | nimble build"
+run "cd /app && nimble update && yes | nimble build"
 
 entrypoint "/entrypoint.sh"
 cmd "./ponyapi"
 
-run %q[rm -rf /root/.nimble /opt ./src/nimcache && apk del libc-dev gcc curl libgcc git perl xz tar]
+run %q[ rm -rf /root/.nimble /opt ./src/nimcache && apk del libc-dev gcc curl libgcc git perl xz tar nim-compiler-deps ]
 
 flatten
 
-run "apk add --no-cache pcre"
+run "apk add --no-cache pcre backplane-runit"
 
 tag "xena/ponyapi"
